f29c2d39a269f88dfbc3890c55e1f678bb8936c9
David Marchante <davidmarchan@gmail.com>
2019-05-13 11:03:43 +0200

    Import index files with the folder name

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-const formats = ['.js', '.json']
+const formats = ['.js', '.json'];
 
-module.exports = (dir) => (
-  fs.readdirSync(dir).reduce((obj, filename) => {
+module.exports = (dirname) => {
+  const dir = path.parse(dirname);
+
+  return fs.readdirSync(dirname).reduce((obj, filename) => {
     const file = path.parse(filename);
+    const requirename = file.name === 'index' ? dir.name : file.name;
 
     return formats.includes(file.ext)
-      ? { ...obj, [file.name]: require(path.join(__dirname, dir, filename)) }
+      ? { ...obj, [requirename]: require(path.join(__dirname, dirname, filename)) }
       : obj;
-  }, {})
-);
+  }, {});
+};
